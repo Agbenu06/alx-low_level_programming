11698bfae13d54e273542f88768871203a871809
Agbenu06 <obandeagbenu2017@gmail.com>
2022-10-13 15:26:11 -0700

    First C file

---
+++ 0x00-hello_world/0-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+	printf("Starting C");
+
+	    return (0);
+
+}

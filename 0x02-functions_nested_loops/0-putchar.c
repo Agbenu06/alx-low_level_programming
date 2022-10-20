4ff41188d0331f51bb15552229c0194b5b805d93
Agbenu06 <obandeagbenu2017@gmail.com>
2022-10-20 02:21:13 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/* Return: Always 0 (success) */
+int main(void)
+{
+	char *sh = "main";
+
+	while (*sh)
+	{
+		_putchar(*sh)
+		sh++;
+
+	}
+	_putchar('/n')
+	
+	return (0);
+}
+

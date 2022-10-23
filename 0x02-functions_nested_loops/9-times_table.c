75236c1fab82ac7579a191a11dae276e5f407357
Agbenu06 <obandeagbenu2017@gmail.com>
2022-10-23 08:42:02 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * times_table - prints the nine times table
+ *
+ * Return: 0
+ */
+
+void times_table(void)
+{
+	int h, i,  j, k, l
+
+	for (h = 0; h < 10; h++)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			j = i * j
+			k = j / 10;
+			l = j % 10;
+
+			if (i == 0)
+				_putchar('0');
+			else if (j < 10)
+			{
+				_putchar(' ');
+				_pitchar('0' + l);
+			}
+			else
+			{
+				_putchar('0' + k);
+				_putchar('0' + l);
+			}
+			if (i < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+				_putchar('\n');
+	}
+}
+
+
+

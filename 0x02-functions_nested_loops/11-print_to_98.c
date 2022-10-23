e6012d97b489c1871f44a78e89c4e45d174087a6
Agbenu06 <obandeagbenu2017@gmail.com>
2022-10-23 14:43:02 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,33 +8,13 @@
 
 void print_to_98(int n)
 {
-	if (n < 98)
+	while (n != 98)
 	{
-		while (n <= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n++;
-		}
-	}
-	else if (n > 98)
-	{
-		while (n >= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
+		printf("%i, ", n);
+		if (n > 98)
 			n--;
-		}
+		else
+			n++;
 	}
-	else
-	{
-		printf("98");
-	}
-	printf("\n");
+	printf("98\n");
 }
